bcb1377b3b404dbc2abf8ad907e2f23c1fbc1b0c
bivhunter <bivhunter@ukr.net>
2019-07-21 20:46:13 +0300

    delete dist/ files (created by babel)

--- dist/main.js
+++
@@ -1,7 +0,0 @@
-import { Game } from "./game";
-import $ from "jquery"; //Запуск гри\
-
-$(document).ready(() => {
-  console.log("main");
-  let gameLounch = new Game($("#game-field"), $("#header-field"));
-});
